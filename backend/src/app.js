f694477f31df23e0ffc67d427e68292a313352ac
IanGf <ngureian64@gmail.com>
2026-03-25 11:49:54 +0300

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -41,7 +41,7 @@ app.use((req, res, next) => {
 
 //CORS cross-origin....
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'https://localhost:5173/',
+    origin: process.env.CLIENT_URL || 'https://localhost:5173',
     credentials: true,
     optionSuccessStatus: 200
 }));
